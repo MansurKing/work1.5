890c15f885b462598ec40733f419b5148fd4b5a5
MansurKing <102825225+MansurKing@users.noreply.github.com>
2022-04-05 19:31:34 +0300

    init

---
+++ work1.5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace work1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 7;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            b = a + b;
+            a = b - a;
+            b = b - a;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
